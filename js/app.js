327d878d54e004e77b0af237a83a7155ebdad52b
Aisha <aishaprograms@gmail.com>
2017-01-15 23:44:51 -0600

    Added weather icons

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getWeather);
-    } else {
-        console.log('Geolocation is not supported by this browser.');
-    }
-}
-
 function getWeather(position) {
     var query = '?lat=' + position.coords.latitude + '&lon=' +
         position.coords.longitude;
@@ -23,8 +15,47 @@ function getWeather(position) {
         $('.loc-name').text(response.name);
         $('.loc-temp').text(response.main.temp + ' °F');
         $('.loc-condition').text(response.weather[0].description);
-
+        var locIcon = $('<i>');
+        var iconClass;
+        switch (response.weather[0].description) {
+            case 'clear sky':
+                iconClass = "wi wi-day-sunny";
+                break;
+            case 'few clouds':
+                iconClass = "wi wi-day-cloudy";
+                break;
+            case 'scattered clouds':
+                iconClass = "wi wi-cloud";
+                break;
+            case 'broken clouds':
+                iconClass = "wi wi-cloudy";
+                break;
+            case 'shower rain':
+                iconClass = "wi wi-rain";
+                break;
+            case 'rain':
+                iconClass = "wi wi-day-showers";
+                break;
+            case 'thunderstorm':
+                iconClass = "wi wi-thunderstorm";
+                break;
+            case 'snow':
+                iconClass = "wi wi-snow";
+                break;
+            case 'mist':
+                iconClass = "wi wi-dust";
+        }
+        locIcon.addClass(iconClass);
+        $('.loc-icon').html(locIcon);
     });
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getWeather);
+    } else {
+        console.log('Geolocation is not supported by this browser.');
+    }
 }
 
 getLocation();
