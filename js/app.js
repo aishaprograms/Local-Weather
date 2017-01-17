b53aee4f40ec47dceaf6b14ef61b40e7b004668f
Aisha <aishaprograms@gmail.com>
2017-01-16 22:32:26 -0600

    Added Celsius and adjusted CSS added Heroku

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
+function convertCtoF(C) {
+    var Fah = parseInt(C) * 9 / 5 + 32;
+    return Math.round(Fah);
+}
+
+function addWeatherIcon(description) {
+    var locIcon = $('<i>');
+    var iconClass;
+    switch (description) {
+        case 'clear sky':
+            iconClass = "wi wi-day-sunny";
+            break;
+        case 'few clouds':
+            iconClass = "wi wi-day-cloudy";
+            break;
+        case 'scattered clouds':
+            iconClass = "wi wi-cloud";
+            break;
+        case 'broken clouds':
+            iconClass = "wi wi-cloudy";
+            break;
+        case 'shower rain':
+            iconClass = "wi wi-rain";
+            break;
+        case 'rain':
+            iconClass = "wi wi-day-showers";
+            break;
+        case 'thunderstorm':
+            iconClass = "wi wi-thunderstorm";
+            break;
+        case 'snow':
+            iconClass = "wi wi-snow";
+            break;
+        case 'mist':
+            iconClass = "wi wi-dust";
+            break;
+        default:
+            iconClass = "wi wi-cloud";
+    }
+    locIcon.addClass(iconClass);
+    return locIcon;
+}
+
 function getWeather(position) {
     var query = '?lat=' + position.coords.latitude + '&lon=' +
         position.coords.longitude;
@@ -6,46 +49,14 @@ function getWeather(position) {
 
     //the URL to query the database
     var queryURL = 'http://api.openweathermap.org/data/2.5/weather' + query +
-        '&units=imperial&appid=' + APIKey;
+        '&units=metric&appid=' + APIKey;
 
-    $.ajax({ url: queryURL, method: 'GET' })
-
-    .done(function(response) {
-        console.log(response);
+    $.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
         $('.loc-name').text(response.name);
-        $('.loc-temp').text(response.main.temp + ' °F');
+        var Fah = convertCtoF(response.main.temp);
+        $('.loc-temp').text(response.main.temp + ' °C / ' + Fah + ' °F');
         $('.loc-condition').text(response.weather[0].description);
-        var locIcon = $('<i>');
-        var iconClass;
-        switch (response.weather[0].description) {
-            case 'clear sky':
-                iconClass = "wi wi-day-sunny";
-                break;
-            case 'few clouds':
-                iconClass = "wi wi-day-cloudy";
-                break;
-            case 'scattered clouds':
-                iconClass = "wi wi-cloud";
-                break;
-            case 'broken clouds':
-                iconClass = "wi wi-cloudy";
-                break;
-            case 'shower rain':
-                iconClass = "wi wi-rain";
-                break;
-            case 'rain':
-                iconClass = "wi wi-day-showers";
-                break;
-            case 'thunderstorm':
-                iconClass = "wi wi-thunderstorm";
-                break;
-            case 'snow':
-                iconClass = "wi wi-snow";
-                break;
-            case 'mist':
-                iconClass = "wi wi-dust";
-        }
-        locIcon.addClass(iconClass);
+        var locIcon = addWeatherIcon(response.weather[0].description);
         $('.loc-icon').html(locIcon);
     });
 }
@@ -57,5 +68,4 @@ function getLocation() {
         console.log('Geolocation is not supported by this browser.');
     }
 }
-
 getLocation();
